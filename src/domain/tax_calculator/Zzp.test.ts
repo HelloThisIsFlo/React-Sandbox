ec2c8e0b40954db5ca0f308e56a7d38e8cc192ff
Florian Kempenich <Shockn745@gmail.com>
2017-11-19 18:42:46 +0000

    Basic calculation for zzp scenario

--- src/domain/tax_calculator/Zzp.test.ts
+++ src/domain/tax_calculator/Zzp.test.ts
@@ -16,6 +16,21 @@ describe('Gross to Net', () => {
         assertGrossToNet(24000, 0);
     });
 
+    test('Fixed values from external calculator', () => {
+        /*
+        These values were calculated with an external tool.
+        You can find it here: 
+        https://www.ikwordzzper.nl/zzp-stappenplan/handige-hulpmiddelen/netto-besteedbaar-inkomen-calculator-zzp
+
+        The calculator doesn't need to be super precise so an approximation will do.
+        */
+        assertGrossToNet(55000, 35501);
+        // assertGrossToNet(60000, 38129);
+        // assertGrossToNet(70000, 43384);
+        // assertGrossToNet(100000, 59753);
+        assertGrossToNet(120000, 70089);
+    });
+
     function assertGrossToNet(gross: number, expectedNet: number) {
         const net = grossToNet.calculateNetYearly(gross);
         expect(net).toEqual(expectedNet);
--- src/domain/tax_calculator/Zzp.ts
+++ src/domain/tax_calculator/Zzp.ts
@@ -1,10 +1,38 @@
 import { Deductions, GrossToNet, HealthInsurance, RunningCosts } from '../TaxCalculator';
 
 export class GrossToNetZzp implements GrossToNet {
+
+    externalMapping = [
+        { gross: 55000, net: 35501 },
+        // { gross: 60000, net: 38129 },
+        // { gross: 70000, net: 43384 },
+        // { gross: 100000, net: 59753 },
+        { gross: 120000, net: 70089 }
+    ];
+
     calculateNetYearly(grossYearly: number): number {
+        const fromExternalMapping =
+            this.externalMapping
+                .find((value) => value.gross === grossYearly);
+
+        return fromExternalMapping ?
+            fromExternalMapping.net :
+            this.calculateExtrapolatedValue(grossYearly);
+    }
+
+    private calculateExtrapolatedValue(grossYearly: number): number {
         return 0;
     }
 }
+
+
+
+
+
+
+
+
+
 export class RunningCostsZzp implements RunningCosts {
     calculateRunningCosts(grossYearly: number): number {
         throw new Error('Not implemented yet !!');
