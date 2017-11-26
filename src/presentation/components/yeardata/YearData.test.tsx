421aba34d2e767d969add2b3a3d751e1ca7507f4
Florian Kempenich <Shockn745@gmail.com>
2017-11-26 15:47:02 +0000

    Add render tests

--- src/presentation/components/yeardata/YearData.test.tsx
+++ src/presentation/components/yeardata/YearData.test.tsx
@@ -1,9 +1,10 @@
 import YearData from './YearData';
 import * as React from 'react';
 import { Config } from '../../SimulationConfig';
-import { shallow, mount } from 'enzyme';
+import { shallow, mount, ShallowWrapper } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import { MuiThemeProvider } from 'material-ui/styles';
+import { ValueSliderProps } from '../valueslider/ValueSlider';
 
 const MOCK_CONFIG: Config = {
     hourlyRate: {
@@ -28,20 +29,83 @@ const MOCK_CONFIG: Config = {
     }
 };
 
-it('should render correctly', () => {
+type SliderName = 'hourlyRate' | 'hoursPerDay' | 'daysPerMonth' | 'monthsPerYear';
+function getValueSlider(yearData: ShallowWrapper, name: SliderName): ShallowWrapper {
+    const position = {
+        hourlyRate: 0,
+        hoursPerDay: 1,
+        daysPerMonth: 2,
+        monthsPerYear: 3
+    };
+    return yearData.childAt(position[name]);
+}
+
+describe('YearData', () => {
     const moneyMadeCallback = jest.fn();
     const yearDataJsx = (
-        <MuiThemeProvider>
-            <YearData
-                onNewMoneyMadeInAYear={moneyMadeCallback}
-                hourlyRate={MOCK_CONFIG.hourlyRate}
-                hoursPerDay={MOCK_CONFIG.hoursPerDay}
-                daysPerMonth={MOCK_CONFIG.daysPerMonth}
-                monthsPerYear={MOCK_CONFIG.monthsPerYear}
-            />
-        </MuiThemeProvider>
+        <YearData
+            onNewMoneyMadeInAYear={moneyMadeCallback}
+            hourlyRate={MOCK_CONFIG.hourlyRate}
+            hoursPerDay={MOCK_CONFIG.hoursPerDay}
+            daysPerMonth={MOCK_CONFIG.daysPerMonth}
+            monthsPerYear={MOCK_CONFIG.monthsPerYear}
+        />
     );
-    const wrapper = mount(yearDataJsx);
+    beforeEach(moneyMadeCallback.mockClear);
 
-    expect(toJson(wrapper)).toMatchSnapshot();
+    describe('General layout', () => {
+        it('should render correctly', () => {
+            const wrapper = mount((
+                <MuiThemeProvider>
+                    {yearDataJsx}
+                </MuiThemeProvider>
+            ));
+            expect(toJson(wrapper)).toMatchSnapshot();
+        });
+
+        it('has the sliders in the correct order', () => {
+            const wrapper = shallow(yearDataJsx);
+            const hourlyRateSlider = getValueSlider(wrapper, 'hourlyRate');
+            const hoursPerDaySlider = getValueSlider(wrapper, 'hoursPerDay');
+            const daysPerMonthSlider = getValueSlider(wrapper, 'daysPerMonth');
+            const monthsPerYearSlider = getValueSlider(wrapper, 'monthsPerYear');
+
+            ensureCaptionOf(hourlyRateSlider).contains('h. rate');
+            ensureCaptionOf(hoursPerDaySlider).contains('h. day');
+            ensureCaptionOf(daysPerMonthSlider).contains('d. month');
+            ensureCaptionOf(monthsPerYearSlider).contains('month');
+
+            function ensureCaptionOf(valueSlider: ShallowWrapper) {
+                const mainCaption =
+                    (valueSlider.props() as ValueSliderProps).mainCaption;
+                return {
+                    contains: (expected: string) => {
+                        expect(mainCaption).toContain(expected);
+                    }
+                };
+            }
+        });
+
+    });
+
+    describe('Calculate Gross Money Made in Year', () => {
+
+        it.skip('test', () => {
+            // const wrapper = shallow(yearDataJsx);
+
+            // console.log(wrapper.childAt(0).debug());
+            // // wrapper.props().onNewMoneyMadeInAYear(44);
+
+            // // wrapper.state().onNewMoneyMadeInAYear(44);
+
+            // let thing;
+            // thing = wrapper.childAt(0).props().onNewValue(44);
+
+            // console.log(thing);
+
+            // console.log(moneyMadeCallback.mock);
+
+        });
+    });
+
 });
