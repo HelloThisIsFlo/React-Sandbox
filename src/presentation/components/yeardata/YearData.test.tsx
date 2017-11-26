57108fcd14f47b38cb57bc7fc236f1ec7f1c9900
Florian Kempenich <Shockn745@gmail.com>
2017-11-26 14:57:04 +0000

    Create snapshot for YearData

--- src/presentation/components/yeardata/YearData.test.tsx
+++ src/presentation/components/yeardata/YearData.test.tsx
@@ -1,19 +1,47 @@
 import YearData from './YearData';
 import * as React from 'react';
-import { CONFIG } from '../../SimulationConfig';
+import { Config } from '../../SimulationConfig';
+import { shallow, mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import { MuiThemeProvider } from 'material-ui/styles';
 
-test('hello', () => {
+const MOCK_CONFIG: Config = {
+    hourlyRate: {
+        min: 20,
+        max: 80,
+        default: 30
+    },
+    hoursPerDay: {
+        min: 2,
+        max: 10,
+        default: 6
+    },
+    daysPerMonth: {
+        min: 5,
+        max: 25,
+        default: 10
+    },
+    monthsPerYear: {
+        min: 2,
+        max: 12,
+        default: 6
+    }
+};
 
+it('should render correctly', () => {
     const moneyMadeCallback = jest.fn();
     const yearDataJsx = (
-        <YearData
-            onNewMoneyMadeInAYear={moneyMadeCallback}
-            hourlyRate={CONFIG.hourlyRate}
-            hoursPerDay={CONFIG.hoursPerDay}
-            daysPerMonth={CONFIG.daysPerMonth}
-            monthsPerYear={CONFIG.monthsPerYear}
-        />
+        <MuiThemeProvider>
+            <YearData
+                onNewMoneyMadeInAYear={moneyMadeCallback}
+                hourlyRate={MOCK_CONFIG.hourlyRate}
+                hoursPerDay={MOCK_CONFIG.hoursPerDay}
+                daysPerMonth={MOCK_CONFIG.daysPerMonth}
+                monthsPerYear={MOCK_CONFIG.monthsPerYear}
+            />
+        </MuiThemeProvider>
     );
+    const wrapper = mount(yearDataJsx);
 
+    expect(toJson(wrapper)).toMatchSnapshot();
 });
-
